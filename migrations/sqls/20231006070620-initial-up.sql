2e266a4d1249f4c2b63aade182c7cc8914276d81
nain4884 <nain4884@gmail.com>
2023-10-06 15:21:51 +0530

    migration update

--- migrations/sqls/20231006070620-initial-up.sql
+++ migrations/sqls/20231006070620-initial-up.sql
@@ -1,25 +1,25 @@
 /* Replace with your SQL commands */
 
 CREATE TABLE IF NOT EXISTS public.users (
-  id SERIAL PRIMARY KEY,
-  userName VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "userName" VARCHAR(255) NOT NULL,
+  "password" VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.match (
-  id SERIAL PRIMARY KEY,
-  marketId VARCHAR(255) UNIQUE NOT NULL,
-  eventId VARCHAR(255) UNIQUE NOT NULL,
-  competitionId VARCHAR(255) NOT NULL,
-  competitionName VARCHAR(255) NOT NULL,
-  gameType VARCHAR(50) NOT NULL,
-  teamA VARCHAR(40) NOT NULL,
-  teamB VARCHAR(40) NOT NULL,
-  teamC VARCHAR(40),
-  title VARCHAR(50) NOT NULL,
-  startDate TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  stopAt TIMESTAMPTZ,
-  overType VARCHAR(255),
-  noBallRun INT
+  "id" SERIAL PRIMARY KEY,
+  "marketId" VARCHAR(255) UNIQUE NOT NULL,
+  "eventId" VARCHAR(255) UNIQUE NOT NULL,
+  "competitionId" VARCHAR(255) NOT NULL,
+  "competitionName" VARCHAR(255) NOT NULL,
+  "gameType" VARCHAR(50) NOT NULL,
+  "teamA" VARCHAR(40) NOT NULL,
+  "teamB" VARCHAR(40) NOT NULL,
+  "teamC" VARCHAR(40),
+  "title" VARCHAR(50) NOT NULL,
+  "startDate" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  "stopAt" TIMESTAMPTZ,
+  "overType" VARCHAR(255),
+  "noBallRun" INT
 );
 
