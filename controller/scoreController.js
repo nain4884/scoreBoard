3f7cc7430f8da4c86d6f0912158eee9bd74d58af
nain4884 <nain4884@gmail.com>
2023-10-17 13:49:18 +0530

    remove duplictea

--- controller/scoreController.js
+++ controller/scoreController.js
@@ -663,7 +663,6 @@ app.post(
     newInning.startAt = new Date();
     newInning.startAt = newInning.startAt.toString();
     newInning.stopAt = newInning.stopAt?.toString() || '';
-    newInning.stopAt = newInning.stopAt?.toString() || '';
     await redisClient.hSet(marketId + "Inning1", newInning);
     matchRepo.update(
       { marketId: marketId },
