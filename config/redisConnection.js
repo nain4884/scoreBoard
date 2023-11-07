ff7beb560c5a07fd9c69b1eefdddcd06b23c5cc8
nain4884 <nain4884@gmail.com>
2023-11-07 10:08:47 +0530

    add redis in env

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -1,8 +1,8 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-    host: "localhost",
-    port: 6379,
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
 });
 redisClient.connect().then(console.log("connect redis")).catch(console.error);
 
