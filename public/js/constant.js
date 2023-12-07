79a6d3708ac2b9e6ca2374991d6592a891f6741f
rohanduple <150104513+rohanduple@users.noreply.github.com>
2023-12-07 17:29:10 +0530

    added wicket in no ball

--- public/js/constant.js
+++ public/js/constant.js
@@ -7,9 +7,9 @@ const API_BASE_URL = protocol + "//" + host;
 const ballEventKeys = {
   b: { key: "b", name: "Ball", validKeys: [], directLive: false }, //ball with score
   w: { key: "w", name: "Wide Ball", validKeys: [], directLive: false }, //wide ball
-  n: { key: "n", name: "No Ball", validKeys: ["r"], directLive: false }, //no ball
+  n: { key: "n", name: "No Ball", validKeys: ["r","wck"], directLive: false }, //no ball
   r: { key: "r", name: "Run out", validKeys: ["n"], directLive: false }, //run out
-  o: { key: "wck", name: "Wicket", validKeys: [], directLive: false }, //wicket
+  o: { key: "wck", name: "Wicket", validKeys: ["n"], directLive: false }, //wicket
   d: { key: "d", name: "Drink Break", validKeys: [], directLive: true }, //drink break
   t: { key: "timeout", name: "Timeout", validKeys: [], directLive: true }, //timeout
   u: { key: "u", name: "Third Umpire", validKeys: [], directLive: true }, //third umpire
